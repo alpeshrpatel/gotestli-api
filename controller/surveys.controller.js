bfd71351d84e48b7ae4d38a2dacd3d847c4f9ae4
Djkarmur <dipakkarmur45@gmail.com>
2024-10-22 23:57:58 +0530

    [quiz]: rating calculation api added

--- controller/surveys.controller.js
+++ controller/surveys.controller.js
@@ -33,19 +33,3 @@ exports.create = (req, res) => {
     else res.send(data);
   });
 };
-
-exports.getRating =  (req, res) => {
-  Surveys.getRating(req.params.id, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.send({
-          message: `Not found rating with id ${req.params.id}.`
-        });
-      } else {
-        res.status(500).send({
-          message: "Error retrieving rating with id " + req.params.id
-        });
-      }
-    } else res.send(data);
-  });
-};
--- routes/surveys.route.js
+++ routes/surveys.route.js
@@ -5,9 +5,6 @@ module.exports = app => {
   
     // Create a new Surveys
     router.post("/", Surveys.create);
-
-    //rating calculation of one questionset
-    router.get('/rating/qset/:id',Surveys.getRating);
   
   
     app.use('/api/surveys', router);
