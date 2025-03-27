2ef2a8e00a9bf7b13baac559ebae0e6425c579a6
Dipak_Karmur <93463692+Djkarmur@users.noreply.github.com>
2025-03-27 10:05:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const app = express();
 // }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://gotestli.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   
